4a6debff9c71ef33fa119496d9dac5c733269ec4
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-12-10 11:56:27 +0000

    deadly cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
-import express from "express";
+import cors from "cors";
 import axios from "axios";
+import express from "express";
 
-const app = express()
-const port = 3000
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+const port = 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  console.log(req);
+app.post("/", (req, res) => {
+  console.log(req.body);
 
   axios
     .post(
@@ -15,7 +24,7 @@ app.get("/", (req, res) => {
       req.body,
       {
         headers: {
-          "x-api-key": process.env.TOLLGURU_API_KEY, 
+          "x-api-key": process.env.TOLLGURU_API_KEY,
           "Content-Type": "application/json",
         },
       }
@@ -30,5 +39,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log("Started!");
-})
+  console.log("Started!");
+});
